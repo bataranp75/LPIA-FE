5065da04fe4b34715770276bfe9edb085109016c
Penny Stuart Keith <masterh475@gmail.com>
2026-08-24 19:31:39 +0700

    fix: integrate tailwind and rebuild with env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   output: 'server',
+
   adapter: node({
     mode: 'standalone',
   }),
+
+  integrations: [tailwind()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
