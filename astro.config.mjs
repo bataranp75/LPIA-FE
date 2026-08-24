b06dbcb657467c320cac4ae1ab65b43de4cc7408
Penny Stuart Keith <masterh475@gmail.com>
2026-08-24 18:37:43 +0700

    fix: add node adapter to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  // Sembunyikan Astro Dev Toolbar
-  devToolbar: {
-    enabled: false,
-  },
   output: 'server',
-  server: {
-    port: 5555, // Mengubah port ke 5555
-  },
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
